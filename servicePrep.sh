5f1127f8bf534fd4660c72ce764adaa6e58c8d0a
turbinex-admin <ms@turbinex.io>
2022-08-03 11:47:58 -0500

    Updating the servicePrep urls.

--- servicePrep.sh
+++ servicePrep.sh
@@ -2,6 +2,15 @@
 
 services=( ihelp-app ihelp-contracts )
 
+# test your github connection
+ssh git@github.com > /dev/null 2>&1
+ec=$?
+echo $ec
+if [[ "$ec" != "1" ]];then
+    echo "Please add your ssh public key to your github account so you can pull the ihelp repos down..."
+    exit 1
+fi
+
 version_tag="master"
 
 for service in "${services[@]}"
@@ -9,8 +18,7 @@ do
 
    echo $service
 
-    git clone https://github.com/iHelp-Finance/$service.git $service
-    
+    git clone git@github.com:ihelp-finance/$service.git $service
     cd $service
     git checkout $version_tag
     cd ../
